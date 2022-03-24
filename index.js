635fab46d0a3d753968e1707de39cbb821813d8a
powermikee <powermikee@gmail.com>
2022-03-24 23:31:39 +1100

    Update index.js

--- index.js
+++ index.js
@@ -50,35 +50,34 @@ async function handleRequest(event) {
     )
     
     function csvJSON(csv){
+      const lines = csv.split("\n");
 
-      var lines=csv.split("\n");
-
-      var result = [];
+      const resultJson = [];
 
       // NOTE: If your columns contain commas in their values, you'll need
       // to deal with those before doing the next step 
       // (you might convert them to &&& or something, then covert them back later)
       // jsfiddle showing the issue https://jsfiddle.net/
-      var headers=lines[0].split(",");
+      const headers = lines[0].split(",");
 
-      for(var i=1;i<lines.length;i++){
+      for (var i = 1; i < lines.length; i++){
+          const obj = {};
+          const currentline = lines[i].split(",");
 
-          var obj = {};
-          var currentline=lines[i].split(",");
-
-          for(var j=0;j<headers.length;j++){
+          for(var j = 0; j < headers.length; j++){
               obj[headers[j]] = currentline[j];
           }
 
-          result.push(obj);
-
+          resultJson.push(obj);
       }
 
       //return result; //JavaScript object
-      return JSON.stringify(result); //JSON
+      return JSON.stringify(resultJson); //JSON
     }
     
     const json = csvJSON(result);
+    
+    console.log(json, result);
 
     let newResponse = new Response(json, {
       status: 200,
